787922051239da6fa8c6aa22cd57fa2786d70b30
larukedi <eser@sent.com>
2014-05-03 06:34:28 +0300

    * added Io::getMimetype method. * added Objects::Binder class.

--- src/Helpers/Io.php
+++ src/Helpers/Io.php
@@ -508,4 +508,174 @@ class Io
             }
         }
     }
+
+    /**
+     * Returns the mimetype of given extension
+     *
+     * @param string $uExtension extension of the file
+     * @param string $uDefault   default mimetype if nothing found
+     *
+     * @return string
+     */
+    public static function getMimetype($uExtension, $uDefault = "application/octet-stream")
+    {
+        $tExtension = String::toLower($uExtension);
+
+        if ($tExtension === "pdf") {
+            return "application/pdf";
+        } elseif ($tExtension === "exe") {
+            return "application/octet-stream";
+        } elseif ($tExtension === "dll") {
+            return "application/x-msdownload";
+        } elseif ($tExtension === "zip") {
+            return "application/zip";
+        } elseif ($tExtension === "rar") {
+            return "application/x-rar-compressed";
+        } elseif ($tExtension === "gz" || $tExtension === "gzip" || $tExtension === "tgz") {
+            return "application/gzip";
+        } elseif ($tExtension === "7z") {
+            return "application/x-7z-compressed";
+        } elseif ($tExtension === "tar") {
+            return "application/x-tar";
+        } elseif ($tExtension === "jar") {
+            return "application/java-archive";
+        } elseif ($tExtension === "deb") {
+            return "application/x-deb";
+        } elseif ($tExtension === "img") {
+            return "application/x-apple-diskimage";
+        } elseif ($tExtension === "csv") {
+            return "text/csv";
+        } elseif ($tExtension === "txt" || $tExtension === "text" || $tExtension === "log" || $tExtension === "ini") {
+            return "text/plain";
+        } elseif ($tExtension === "md") {
+            return "text/x-markdown";
+        } elseif ($tExtension === "rtf") {
+            return "text/rtf";
+        } elseif ($tExtension === "odt") {
+            return "application/vnd.oasis.opendocument.text";
+        } elseif ($tExtension === "ods") {
+            return "application/vnd.oasis.opendocument.spreadsheet";
+        } elseif ($tExtension === "smil") {
+            return "application/smil";
+        } elseif ($tExtension === "eml") {
+            return "message/rfc822";
+        } elseif ($tExtension === "xml" || $tExtension === "xsl") {
+            return "text/xml";
+        } elseif ($tExtension === "doc" || $tExtension === "dot" || $tExtension === "word") {
+            return "application/msword";
+        } elseif ($tExtension === "docx" || $tExtension === "dotx") {
+            return "application/vnd.openxmlformats-officedocument.wordprocessingml.document";
+        } elseif ($tExtension === "xls") {
+            return "application/vnd.ms-excel";
+        } elseif ($tExtension === "xl") {
+            return "application/excel";
+        } elseif ($tExtension === "xlsx") {
+            return "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+        } elseif ($tExtension === "ppt" || $tExtension === "pps") {
+            return "application/vnd.ms-powerpoint";
+        } elseif ($tExtension === "pptx") {
+            return "application/vnd.openxmlformats-officedocument.presentationml.presentation";
+        } elseif ($tExtension === "ppsx") {
+            return "application/vnd.openxmlformats-officedocument.presentationml.slideshow";
+        } elseif ($tExtension === "ics") {
+            return "text/calendar";
+        } elseif ($tExtension === "vcf" || $tExtension === "vcard") {
+            return "text/vcard";
+        } elseif ($tExtension === "bmp") {
+            return "image/x-ms-bmp";
+        } elseif ($tExtension === "gif") {
+            return "image/gif";
+        } elseif ($tExtension === "png") {
+            return "image/png";
+        } elseif ($tExtension === "jpeg" || $tExtension === "jpe" || $tExtension === "jpg") {
+            return "image/jpeg";
+        } elseif ($tExtension === "webp") {
+            return "image/webp";
+        } elseif ($tExtension === "tif" || $tExtension === "tiff") {
+            return "image/tiff";
+        } elseif ($tExtension === "psd") {
+            return "image/vnd.adobe.photoshop";
+        } elseif ($tExtension === "ai" || $tExtension === "eps" || $tExtension === "ps") {
+            return "application/postscript";
+        } elseif ($tExtension === "cdr") {
+            return "application/cdr";
+        } elseif ($tExtension === "mid" || $tExtension === "midi") {
+            return "audio/midi";
+        } elseif ($tExtension === "mpga" || $tExtension === "mp2" || $tExtension === "mp3") {
+            return "audio/mpeg";
+        } elseif ($tExtension === "aif" || $tExtension === "aiff" || $tExtension === "aifc") {
+            return "audio/x-aiff";
+        } elseif ($tExtension === "wav") {
+            return "audio/x-wav";
+        } elseif ($tExtension === "aac") {
+            return "audio/aac";
+        } elseif ($tExtension === "ogg") {
+            return "application/ogg";
+        } elseif ($tExtension === "wma") {
+            return "audio/x-ms-wma";
+        } elseif ($tExtension === "m4a") {
+            return "audio/x-m4a";
+        } elseif ($tExtension === "mpeg" || $tExtension === "mpg" || $tExtension === "mpe") {
+            return "video/mpeg";
+        } elseif ($tExtension === "mp4" || $tExtension === "f4v") {
+            return "application/mp4";
+        } elseif ($tExtension === "qt" || $tExtension === "mov") {
+            return "video/quicktime";
+        } elseif ($tExtension === "avi") {
+            return "video/x-msvideo";
+        } elseif ($tExtension === "wmv") {
+            return "video/x-ms-wmv";
+        } elseif ($tExtension === "webm") {
+            return "video/webm";
+        } elseif ($tExtension === "swf") {
+            return "application/x-shockwave-flash";
+        } elseif ($tExtension === "flv") {
+            return "video/x-flv";
+        } elseif ($tExtension === "mkv") {
+            return "video/x-matroska";
+        } elseif ($tExtension === "htm" || $tExtension === "html" || $tExtension === "shtm" ||
+            $tExtension === "shtml") {
+            return "text/html";
+        } elseif ($tExtension === "php") {
+            return "application/x-httpd-php";
+        } elseif ($tExtension === "phps") {
+            return "application/x-httpd-php-source";
+        } elseif ($tExtension === "css") {
+            return "text/css";
+        } elseif ($tExtension === "js") {
+            return "application/x-javascript";
+        } elseif ($tExtension === "json") {
+            return "application/json";
+        } elseif ($tExtension === "c" || $tExtension === "h") {
+            return "text/x-c";
+        } elseif ($tExtension === "py") {
+            return "application/x-python";
+        } elseif ($tExtension === "sh") {
+            return "text/x-shellscript";
+        } elseif ($tExtension === "pem") {
+            return "application/x-x509-user-cert";
+        } elseif ($tExtension === "crt" || $tExtension === "cer") {
+            return "application/x-x509-ca-cert";
+        } elseif ($tExtension === "pgp") {
+            return "application/pgp";
+        } elseif ($tExtension === "gpg") {
+            return "application/gpg-keys";
+        } elseif ($tExtension === "svg") {
+            return "image/svg+xml";
+        } elseif ($tExtension === "ttf") {
+            return "application/x-font-ttf:";
+        } elseif ($tExtension === "woff") {
+            return "application/x-font-woff";
+        }
+
+        if (function_exists("finfo_open")) {
+            $tFinfo = finfo_open(FILEINFO_MIME);
+            $tMimetype = finfo_file($tFinfo, "test.{$uExtension}");
+            finfo_close($tFinfo);
+
+            return $tMimetype;
+        }
+
+        return $uDefault;
+    }
 }
---
+++ src/Objects/Binder.php
@@ -0,0 +1,158 @@
+<?php
+/**
+ * Scabbia2 PHP Framework
+ * http://www.scabbiafw.com/
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @link        http://github.com/scabbiafw/scabbia2 for the canonical source repository
+ * @copyright   2010-2013 Scabbia Framework Organization. (http://www.scabbiafw.com/)
+ * @license     http://www.apache.org/licenses/LICENSE-2.0 - Apache License, Version 2.0
+ */
+
+namespace Scabbia\Objects;
+
+use Scabbia\Framework\Core;
+use Scabbia\Helpers\Io;
+
+/**
+ * Binder
+ *
+ * @package     Scabbia\Config
+ * @author      Eser Ozvataf <eser@sent.com>
+ * @since       1.5.0
+ */
+class Binder
+{
+    /** @type string name */
+    public $name = null;
+
+    /** @type string cache path */
+    public $cachepath;
+
+    /** @type bool sealed */
+    public $sealed;
+
+    /** @type array contents */
+    public $contents = [];
+
+    /** @type string output */
+    public $output;
+
+
+    /**
+     * Initializes a Binder class instance
+     *
+     * @param string $uName a name for binder
+     *
+     * @return Binder
+     */
+    public function __construct($uName) {
+        $this->name = $uName;
+
+        $this->cachepath = Core::$basepath . "/writable/cache/" . crc32("binder/{$uName}");
+        $tOptions = [
+            "ttl" => 60 * 60
+        ];
+
+        if ($this->sealed = Io::isReadable($this->cachepath, $tOptions)) {
+            $this->output = Io::readSerialize($this->cachepath);
+        }
+    }
+
+    /**
+     * Adds a content
+     *
+     * @param string $uContent       content
+     * @param string $uMimeType      mimetype of content
+     *
+     * @return void
+     */
+    public function addContent($uContent, $uMimeType = "text/plain")
+    {
+        if ($this->sealed) {
+            return;
+        }
+
+        $this->contents[] = ["direct", $uMimeType, $uContent];
+    }
+
+    /**
+     * Adds a callback
+     *
+     * @param callable $uCallback  callback
+     * @param string   $uMimeType  mimetype of content
+     *
+     * @return void
+     */
+    public function addCallback($uCallback, $uMimeType = "text/plain")
+    {
+        if ($this->sealed) {
+            return;
+        }
+
+        $this->contents[] = ["callback", $uMimeType, $uCallback];
+    }
+
+    /**
+     * Adds a file
+     *
+     * @param string $uPath  path of the content
+     *
+     * @return void
+     */
+    public function addFile($uPath)
+    {
+        if ($this->sealed) {
+            return;
+        }
+
+        $tFilePath = Core::translateVariables($uPath);
+        $tExtension = pathinfo($tFilePath, PATHINFO_EXTENSION);
+        $this->contents[] = ["file", Io::getMimetype($tExtension), $tFilePath];
+    }
+
+    /**
+     * Compiles given configuration files into single configuration
+     *
+     * @param null|string $uMimeTypeFilter only compiles filtered mimetypes
+     *
+     * @return void
+     */
+    public function compile($uMimeTypeFilter = null)
+    {
+        if ($this->sealed) {
+            return;
+        }
+
+        $this->sealed = true;
+        $this->output = "";
+
+        foreach ($this->contents as $tContent) {
+            if ($uMimeTypeFilter !== null && $uMimeTypeFilter !== $tContent[1]) {
+                continue;
+            }
+
+            if ($tContent[0] === "direct") {
+                $this->output .= $tContent[2];
+            } elseif ($tContent[0] === "callback") {
+                $this->output .= call_user_func($tContent[2]);
+            } elseif ($tContent[0] === "file") {
+                $tFileCacheFile = Core::$basepath . "/writable/cache/" . crc32(realpath($tContent[2]));
+
+                $this->output .= Io::readFromCacheFile(
+                    $tFileCacheFile,
+                    function () use ($tContent) {
+                        return Io::read($tContent[2]);
+                    },
+                    [
+                        "ttl" => 60 * 60
+                    ]
+                );
+            }
+        }
+
+        Io::writeSerialize($this->cachepath, $this->output);
+    }
+}

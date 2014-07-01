e382a24d6caa2f51b16c0c34734ea75539f40eb7
larukedi <eser@sent.com>
2014-07-01 18:55:13 +0300

    * Views introduced.

---
+++ src/Views/Views.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Scabbia2 PHP Framework
+ * http://www.scabbiafw.com/
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @link        http://github.com/scabbiafw/scabbia2 for the canonical source repository
+ * @copyright   2010-2013 Scabbia Framework Organization. (http://www.scabbiafw.com/)
+ * @license     http://www.apache.org/licenses/LICENSE-2.0 - Apache License, Version 2.0
+ */
+
+namespace Scabbia\Views;
+
+use Scabbia\Framework\Core;
+use \Exception;
+
+/**
+ * Views
+ *
+ * @package     Scabbia\Views
+ * @author      Eser Ozvataf <eser@sent.com>
+ * @since       1.0.0
+ */
+class Views
+{
+    /** @type array $engines set of engines */
+    public static $engines = [
+        "php" => "self::renderPhpFile"
+    ];
+
+
+    /**
+     * Renders a view
+     *
+     * @param string $uView  view file
+     * @param mixed  $uModel view model
+     *
+     * @throws Exception if any render engine is not associated with the extension
+     */
+    public static function viewFile($uView, $uModel = null)
+    {
+        $tViewFilePath = Core::translateVariables($uView);
+        $tViewFileInfo = pathinfo($tViewFilePath);
+
+        if (!isset(self::$engines[$tViewFileInfo["extension"]])) {
+            // TODO exception
+            throw new Exception("");
+        }
+
+        call_user_func(
+            self::$engines[$tViewFileInfo["extension"]],
+            "{$tViewFileInfo["dirname"]}/",
+            $tViewFileInfo["basename"],
+            $uModel
+        );
+    }
+
+    public static function renderPhpFile($tTemplatePath, $tTemplateFile, $uModel = null)
+    {
+        if ($uModel !== null && is_array($uModel)) {
+            extract($uModel, EXTR_SKIP | EXTR_REFS);
+        }
+
+        include "{$tTemplatePath}{$tTemplateFile}";
+    }
+}

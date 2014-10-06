52cf035d2a2d7f524423121301bc20b1cab5478e
Eser Ozvataf <eser@sent.com>
2014-10-06 03:27:29 +0300

    * docblocks updated.

--- src/Views/Generator.php
+++ src/Views/Generator.php
@@ -1,13 +1,13 @@
 <?php
 /**
- * Scabbia2 PHP Framework
+ * Scabbia2 PHP Framework Code
  * http://www.scabbiafw.com/
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  *
- * @link        http://github.com/scabbiafw/scabbia2 for the canonical source repository
- * @copyright   2010-2013 Scabbia Framework Organization. (http://www.scabbiafw.com/)
+ * @link        http://github.com/scabbiafw/scabbia2-fw for the canonical source repository
+ * @copyright   2010-2014 Scabbia Framework Organization. (http://www.scabbiafw.com/)
  * @license     http://www.apache.org/licenses/LICENSE-2.0 - Apache License, Version 2.0
  */
 
